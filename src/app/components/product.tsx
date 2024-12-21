56be290c6713b9010884225618166f08db9e177d
munibaweb123 <munibavirtualuni@gmail.com>
2024-12-21 20:53:57 +0500

    dependency issue resolving

--- src/app/components/product.tsx
+++ src/app/components/product.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import React from 'react';
+
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 import { FaEye, FaRegHeart } from 'react-icons/fa';
 
