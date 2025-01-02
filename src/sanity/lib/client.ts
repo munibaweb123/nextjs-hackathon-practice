df7e8a05751b2991a3c2febd8243a9144c69c2c0
munibaweb123 <munibavirtualuni@gmail.com>
2025-01-03 00:37:40 +0500

    environment var

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -5,6 +5,6 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,  // The '!' operator is used to assert that these values are not undefined
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: process.env.NEXT_PUBLIC_API_VERSION || '2025-01-02',  // You can set a default API version if the environment variable is not defined
-  token: process.env.NEXT_PUBLIC_API_TOKEN,  // Optional: Include token only if required (e.g., for write operations)
+  token: process.env.SANITY_API_TOKEN,  // Optional: Include token only if required (e.g., for write operations)
   useCdn: true,  // Set to 'false' if you need to disable CDN caching for dynamic data fetching
 });
