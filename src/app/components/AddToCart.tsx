4cc32fb18790db540034262482c70f59584e7ce1
munibaweb123 <munibavirtualuni@gmail.com>
2025-01-05 07:33:16 +0500

    add to cart button design

--- src/app/components/AddToCart.tsx
+++ src/app/components/AddToCart.tsx
@@ -26,6 +26,6 @@ export default function AddToCart({name,description,price,currency,image,id}:Pro
           sku: `${name}-${Math.random().toString(36).substr(2, 9)}`
     }
     return(
-        <Button onClick={()=>{addItem(Product);handleCartClick()}}>Add to Cart</Button>
+        <Button className="hover:bg-[#db4444] w-full hover:text-white shadow-md" onClick={()=>{addItem(Product);handleCartClick()}}>Add to Cart</Button>
     )
 }
